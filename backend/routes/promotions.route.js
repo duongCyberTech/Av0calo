4d89345b7f4c307cd594cf7cb42c4d2fec0bf60d
dCyberTech <maidangduong92tn@gmail.com>
2025-12-03 14:14:49 +0700

    New Service: Promotion Service

--- backend/routes/promotions.route.js
+++ backend/routes/promotions.route.js
@@ -1,8 +1,13 @@
 const PromotionsController = require('../controllers/promotions.controller');
 const express = require('express');
 const router = express.Router();
+const { authen } = require('../middlewares/authentication.middleware');
+const { authorize } = require('../middlewares/authorization.middleware');
 
-router.post('/', PromotionsController.createPromotion);
+// Apply authentication middleware to all promotion routes
+router.use(authen);
+
+router.post('/', authorize(['admin']), PromotionsController.createPromotion);
 router.get('/', PromotionsController.getAllPromotions);
 
 module.exports = router;
